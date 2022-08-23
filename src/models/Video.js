206399f7e353ef70b885b11004fc9e1d6507a898
Youngju-Jang <joj1043@github.com>
2022-08-23 16:03:17 +0900

    ~7.5 session, cookie, loggedin

--- src/models/Video.js
+++ src/models/Video.js
@@ -1,14 +1,26 @@
 import mongoose from "mongoose";
 
 const videoSchema = new mongoose.Schema({
-  title: String,
-  description: String,
-  createdAt: Date,
-  hashtags: [{ type: String }],
-  meta: {
-    views: Number,
-    rating: Number,
+  title: {
+    type: String,
+    trim: true,
+    required: true,
+    uppercase: true,
+    maxLength: 80,
   },
+  description: { type: String, trim: true, required: true, minLength: 10 },
+  createdAt: { type: Date, required: true, default: Date.now },
+  hashtags: [{ type: String, trim: true }],
+  meta: {
+    views: { type: Number, default: 0, required: true },
+    rating: { type: Number, default: 0, required: true },
+  },
+});
+
+videoSchema.static("formatHashtags", function (hashtags) {
+  return hashtags
+    .split(",")
+    .map((word) => (word.startsWith("#") ? word : `#${word}`));
 });
 
 const Video = mongoose.model("Video", videoSchema);
