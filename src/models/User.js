4a9127b84f9978c00e010a2d3283cf9d5780c251
Youngju-Jang <joj1043@github.com>
2022-08-29 01:27:47 +0900

    ~8.10 profile, owner, user's video

--- src/models/User.js
+++ src/models/User.js
@@ -9,10 +9,14 @@ const userSchema = new mongoose.Schema({
   password: { type: String },
   name: { type: String },
   location: String,
+  videos: [{ type: mongoose.Schema.Types.ObjectId, ref: "Video" }],
 });
 
 userSchema.pre("save", async function () {
-  this.password = await bcrypt.hash(this.password, 5);
+  if (this.isModified("password")) {
+    // User의 password가 수정됐을때 true
+    this.password = await bcrypt.hash(this.password, 5);
+  }
 });
 
 const User = mongoose.model("User", userSchema);
