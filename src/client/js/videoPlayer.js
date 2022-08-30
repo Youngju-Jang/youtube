de5b7aee99a7f7a2fe3b8842467579a752d8b5c9
Youngju-Jang <joj1043@github.com>
2022-08-30 17:03:55 +0900

    ~ch11.3 volume

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,7 +1,42 @@
 const video = document.querySelector("video");
-const play = document.getElementById("play");
-const mute = document.getElementById("mute");
+const playBtn = document.getElementById("play");
+const muteBtn = document.getElementById("mute");
 const time = document.getElementById("time");
-const volume = document.getElementById("volume");
+const volumeRange = document.getElementById("volume");
 
-console.log(play, mute, time, volume);
+let volumeValue = 0.5;
+video.volume = volumeValue;
+
+const handlePlayClick = (e) => {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+  playBtn.innerText = video.paused ? "Play" : "Pause";
+};
+
+const handleMute = (e) => {
+  if (video.muted) {
+    video.muted = false;
+  } else {
+    video.muted = true;
+  }
+  muteBtn.innerText = video.muted ? "Unmute" : "Mute";
+  volumeRange.value = video.muted ? 0 : volumeValue;
+};
+
+const handleVolumeChange = (event) => {
+  const {
+    target: { value },
+  } = event;
+  if (video.muted) {
+    video.muted = false;
+    muteBtn.innerText = "Mute";
+  }
+  volumeValue = value;
+  video.volumn = value;
+};
+playBtn.addEventListener("click", handlePlayClick);
+muteBtn.addEventListener("click", handleMute);
+volumeRange.addEventListener("input", handleVolumeChange);
