444e82f0095f65dbca7a59632bb7a328b6dfe495
Youngju-Jang <joj1043@github.com>
2022-09-02 15:31:19 +0900

    ch13. video recorder end

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,8 +1,46 @@
 const startBtn = document.getElementById("startBtn");
 const video = document.getElementById("preview");
 
-const handleStart = async () => {
-  const stream = await navigator.mediaDevices.getUserMedia({
+let stream;
+let recorder;
+let videoFile;
+
+const handleDownload = () => {
+  startBtn.innerText = "Start Recording";
+  const a = document.createElement("a");
+  a.href = videoFile;
+  a.download = "MyRecording.webm";
+  document.body.appendChild(a);
+  a.click();
+};
+
+const handleStop = () => {
+  startBtn.innerText = "Download Recording";
+  startBtn.removeEventListener("click", handleStop);
+  startBtn.addEventListener("click", handleDownload);
+  recorder.stop();
+};
+
+const handleStart = () => {
+  startBtn.innerText = "Stop Recording";
+  startBtn.removeEventListener("click", handleStart);
+  startBtn.addEventListener("click", handleStop);
+
+  recorder = new MediaRecorder(stream, { mimeType: "video/webm" });
+  recorder.ondataavailable = (event) => {
+    videoFile = URL.createObjectURL(event.data);
+    // blob:http://localhost:4000/938d5a5f-4b94-4bf8-ae62-75f4cb083194
+    // >>   브라우저 메모리상에 존재하는 url > 이걸로 파일에 접근가능
+    video.srcObject = null;
+    video.src = videoFile;
+    video.loop = true;
+    video.play();
+  };
+  recorder.start();
+};
+
+const init = async () => {
+  stream = await navigator.mediaDevices.getUserMedia({
     audio: false,
     video: { width: 200, height: 100 },
   });
@@ -10,4 +48,5 @@ const handleStart = async () => {
   video.play();
 };
 
+init();
 startBtn.addEventListener("click", handleStart);
