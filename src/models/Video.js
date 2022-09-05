5c095090eaaf24a3d0ce40f1af0b2d31ee5d723f
Youngju-Jang <joj1043@github.com>
2022-09-05 12:11:10 +0900

    ch14. webassembly video transcode

--- src/models/Video.js
+++ src/models/Video.js
@@ -9,7 +9,8 @@ const videoSchema = new mongoose.Schema({
     maxLength: 80,
   },
   fileUrl: { type: String, required: true },
-  description: { type: String, trim: true, required: true, minLength: 10 },
+  thumbUrl: { type: String, required: true },
+  description: { type: String, trim: true, required: true, minLength: 5 },
   createdAt: { type: Date, required: true, default: Date.now },
   hashtags: [{ type: String, trim: true }],
   meta: {
@@ -23,6 +24,10 @@ videoSchema.static("formatHashtags", function (hashtags) {
   return hashtags
     .split(",")
     .map((word) => (word.startsWith("#") ? word : `#${word}`));
+});
+
+videoSchema.static("changePathFormula", (urlPath) => {
+  return urlPath.replace(/\\/g, "/");
 });
 
 const Video = mongoose.model("Video", videoSchema);
